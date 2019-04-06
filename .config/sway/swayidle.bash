8a0dd218539b21865bb3a21793774f4a8d0fe902
Barry Lapthorn <barry@lapthorn.net>
2019-04-06 18:02:02 +0000

    make the monitor off time longer

--- .config/sway/swayidle.bash
+++ .config/sway/swayidle.bash
@@ -1,6 +1,6 @@
 #!/bin/env bash
 swayidle timeout 180 "~/.config/swaylock/lock.bash 50" \
-timeout 300 'swaymsg "output * dpms off"' \
+timeout 900 'swaymsg "output * dpms off"' \
 resume 'swaymsg "output * dpms on"' \
 before-sleep "~/.config/swaylock/lock.bash 50" &
 
